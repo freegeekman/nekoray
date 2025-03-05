dc5d7937dabf95b9f0f5a96a429cb006b86046ed
freegeekman <169313387+freegeekman@users.noreply.github.com>
2025-03-05 23:46:56 +0100

    minor change

--- libs/get_source.sh
+++ libs/get_source.sh
@@ -2,22 +2,22 @@
 set -e
 
 source libs/env_deploy.sh
-ENV_NEKORAY=1
-source libs/get_source_env.sh
+# ENV_NEKORAY=1
+# source libs/get_source_env.sh
 pushd ..
 
 ####
 if [ ! -d "sing-box-extra" ]; then
   git clone --no-checkout https://github.com/MatsuriDayo/sing-box-extra.git
 fi
-pushd sing-box-extra
+# pushd sing-box-extra
 # git checkout "$COMMIT_SING_BOX_EXTRA"
 
-ENV_SING_BOX_EXTRA=1
-source $SRC_ROOT/libs/get_source_env.sh
-NO_ENV=1 ./libs/get_source.sh
+# ENV_SING_BOX_EXTRA=1
+# source $SRC_ROOT/libs/get_source_env.sh
+# NO_ENV=1 ./libs/get_source.sh
 
-popd
+# popd
 
 ####
 if [ ! -d "Xray-core" ]; then
